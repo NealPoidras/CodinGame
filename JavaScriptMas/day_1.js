903a4583d30ab8511aa6d68a03d135a2b71ded69
NealPoidras <neal.poidras@efrei.net>
2020-12-06 20:02:52 +0100

    delete JavaScriptMas

--- JavaScriptMas/day_1.js
+++
@@ -1,6 +0,0 @@
-function findNumberOfCandies(candies,numberOfChildren)
-{
-    return Math.floor(candies/numberOfChildren)*numberOfChildren;
-}
-
-console.log(findNumberOfCandies(10,3))
--- JavaScriptMas/day_3.js
+++
@@ -1,6 +0,0 @@
-function split_array(myArray,index)
-{
-    return [myArray.slice(0,index), myArray.slice(index)];
-    
-}
-console.log(split_array(['a','b','c','d','e'],3))
