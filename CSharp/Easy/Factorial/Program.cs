ad0d22e2654d05b0446eb63c32b98c31c9eeda97
NealPoidras <neal.poidras@efrei.net>
2021-05-07 09:39:18 +0200

    factorial

---
+++ CSharp/Easy/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+
+        static public int factorial_recursive(int n)
+        {
+            if(n==1){return 1;}
+            else {return n*factorial_recursive(n-1);}
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("in recursive way :"+factorial_recursive(n));
+            int var = 1;
+            for(int i=1;i<=n;i++)
+            {
+                var=var*i;
+            }
+            Console.WriteLine("in loop way:"+var);
+
+        }
+    }
+}
